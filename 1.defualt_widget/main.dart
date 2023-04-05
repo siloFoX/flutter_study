e49cc87c8b9f49b946e6fba4256bca5c3aa678ca
ladnav0704@gmail.com <ladnav0704@gmail.com>
2023-04-05 20:26:55 +0900

    first commit in Defualt widget

---
+++ 1.defualt_widget/main.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build (BuildContext context) {
+    
+    return MaterialApp(
+      home : Scaffold(
+        body : Center(
+          child : IconButton(
+            onPressed : () {},
+              icon : Icon(
+                Icons.home
+                ),
+            ),
+          ),
+        ),
+      );
+  }
+}
